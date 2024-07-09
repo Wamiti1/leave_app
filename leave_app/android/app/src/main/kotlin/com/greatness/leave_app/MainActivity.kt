6696bfe78a091d7b6fa102afdcf9c97b08537bdd
Wamiti1 <davidwamiti11200@gmail.com>
2024-07-09 10:38:47 +0300

    leave_app folder

---
+++ leave_app/android/app/src/main/kotlin/com/greatness/leave_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.greatness.leave_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ leave_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:leave_app/screens/login.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Leave App',
+    debugShowCheckedModeBanner: false,    
+    theme: FlexThemeData.light(scheme: FlexScheme.hippieBlue,),
+    darkTheme: FlexThemeData.dark(scheme: FlexScheme.hippieBlue, darkIsTrueBlack: true),
+    home: const Login(),
+  ));
+}
+
+
